386463fad17fb814203201c642f15c154b455335
Matt Hayes <matt@mysterycommand.com>
2015-11-30 17:34:40 -0500

    Add bootstrap and start dinking around with it.

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const path = require('path');
 const connect = require('connect');
 const serveStatic = require('serve-static');
 
-const DIR = path.join(__dirname, 'static');
+const STATIC_DIR = path.join(__dirname, 'static');
+const BOOTSTRAP_DIR = path.join(__dirname, 'node_modules/bootstrap/dist');
 const PORT = 8080;
 
 connect()
-    .use(serveStatic(DIR))
+    .use(serveStatic(STATIC_DIR))
+    .use(serveStatic(BOOTSTRAP_DIR))
     .listen(PORT);
